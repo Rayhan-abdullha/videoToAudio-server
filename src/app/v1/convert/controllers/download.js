b1687d98d695cfae8c747e0e06a3a8f5a1160e34
Rayhan-abdullha <captainrayhan215@gmail.com>
2023-09-04 18:21:09 +0600

    changes

--- src/app/v1/convert/controllers/download.js
+++ src/app/v1/convert/controllers/download.js
@@ -1,13 +1,7 @@
 const { audioFileName } = require("../../../../config");
 const downloadMp3 = (req, res, next) => {
   try {
-    res.download(audioFileName, (err) => {
-      if (!err) {
-        return res
-          .status(200)
-          .json({ code: 200, message: "Download Successfull" });
-      }
-    });
+    res.download(audioFileName);
   } catch (err) {
     next(err);
   }
